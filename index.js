9a5178ed35bda8d41ef76a09d215a25223e8381a
gzavaglia <gio.zavaglia@gmail.com>
2019-11-21 18:58:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, fn) {
+    let newArr = []
+    for (let i = 0; i < src.length; i++) {
+        const element = fn(src[i])
+        newArr.push(element)
+     }
+     return newArr
+}
+
+function reduce(src, cb, starting){
+    let s = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      s = cb(src[i], s)
+    }
+  
+    return s;
+  }
