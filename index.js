39d7b5aefdaf85a1648251808a7ba5049c9e0869
Michael Chen <michaelchengaming@gmail.com>
2019-11-22 17:18:17 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(array, func){
+    let newArr = [];
+    for (let el of array){
+        newArr.push(func(el));
+    };
+    return newArr;
+};
+
+function reduce(array, func, startingValue){
+    let total = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+        total = func(array[i], total)
+    }
+  
+    return total;
+  }
