238245922e8c7f02a96f5fb9b3c66df90e079adb
Michael Chen <michaelchengaming@gmail.com>
2019-11-22 18:33:12 -0800

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function reduce(array, func, startingValue){
     let total = (!!startingValue) ? startingValue : array[0]
     let i = (!!startingValue) ? 0 : 1
   
-    for (; i < array.length; i++) {
+    for (let el of array) {
         total = func(array[i], total)
     }
   
