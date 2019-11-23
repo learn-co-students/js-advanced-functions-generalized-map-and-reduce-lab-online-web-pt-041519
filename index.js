448e2fe37f477ef6c68e75a30d493240f81525b5
Meg Galvez <meg@oakmaine.com>
2019-11-23 11:04:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(arr, func){
+    let newArr = []
+    arr.forEach(el => newArr.push(func(el)))
+    return newArr
+}
+
+
+function reduce(arr, func, start) {
+    let total = (!!start) ? start : arr[0]
+
+    for(let i = (!!start) ? 0:1; i <arr.length; i++) {
+        total = func(arr[i], total)
+    }
+    return total
+}
