4c33bc4d0fc678bbe21018ac16b68203fd47007d
github username <github email address>
2019-11-27 13:11:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(array, fn) { 
+    return array.map(fn)
+}
+
+function reduce(arr, fn, start){
+    let total = (!!start) ? start : arr[0]
+    
+    for(let i = (!!start) ? 0 : 1; i < arr.length; i++){
+        total = fn(arr[i], total)
+    }
+    return total
+}
