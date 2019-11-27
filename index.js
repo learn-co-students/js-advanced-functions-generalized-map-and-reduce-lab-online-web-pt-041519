a1da1f48f8115b38bf485d4ffd0a912d52a295fa
joannayhs <joannayuhas@gmail.com>
2019-11-27 15:48:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(srcArray, fn){
+    let newArray = []
+    for(let i = 0; i < srcArray.length; i++){
+        newArray.push(fn(srcArray[i]))
+    }
+    return newArray
+}
+
+function reduce(srcArray, fn, start = 0){
+    let total = start ? start : srcArray[0]
+    let i = start ? 0 : 1
+    for(; i < srcArray.length; i++){
+        total = fn(srcArray[i], total)
+    }
+    return total
+
+}
