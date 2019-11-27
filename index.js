01086d33f3b8a41ee3b3bbbfe1f6c325ac9aad36
lanibon <nguyenlan13@gmail.com>
2019-11-26 22:51:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr, fn){
+    let newArr = []
+    for(let i=0; i < arr.length; i++){
+        let el = arr[i]
+        newArr.push(fn(el))
+    }
+    return newArr
+}
+
+function reduce(arr, fn, start){
+    let total = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < arr.length; i++){
+        total = fn(arr[i], total)
+    }
+    return total
+}
