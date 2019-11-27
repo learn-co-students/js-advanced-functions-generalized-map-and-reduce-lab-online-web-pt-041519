bdc08cf463310540832393176aa1f806fa0118a1
earthctzn <earthctzn1@gmail.com>
2019-11-27 16:47:27 -0500

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ function map(srcArray, func){
 }
 
 function reduce(srcArray, callBack, startingPoint){
-  let theBase = (!!startingPoint) ? startingPoint : srcArray[0]
-  let theIndex = (!!startingPoint) ? 0 : 1
-  for (; theIndex < srcArray.length; theIndex++) {
-    theBase = callBack(srcArray[theIndex], theBase)
+  //here we delcare our accumulator as `total`.
+  //Since `startingPoint` is an optional argument we use a conditional. Just like if else.
+  //When a value is passed in, we assign that value to `total`. 
+  //When no value is passed in, we assign the value of the element at index 0 in the `srcArray` 
+  let total = (!!startingPoint) ? startingPoint : srcArray[0]
+  //here we use a conditional again in case `startingPoint` has a value.
+  //if it does have a value we start the iteration at index 0 if there is no value we start at index 1,
+  //this is because we have already assigned the value of the element at index 0 of the `srcArray` to the `total` variable. 
+  for (let i = (!!startingPoint) ? 0 : 1; i < srcArray.length; i++) {
+    //here we are assigning the value of the return of the function being passed in to our `callBack` variable. 
+    //We are also passing in the values of each element in the `srcArray` and the current value of `total`.
+    total = callBack(srcArray[i], total)
   }
-  return theBase
+  //returning the total after iterating over the array elements. 
+  return total
 }
