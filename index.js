a86bfde8ba9f21ec2ba524d05cb337827094e92b
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:43:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArray, sourceFunction) {
+  let arr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    arr.push(sourceFunction(sourceArray[i]));
+  }
+  return arr;
+}
+
+function reduce(sourceArray, sourceFunction, startingValue=0) {
+  let r = (!!startingValue) ? startingValue : sourceArray[0]
+  let i = (!!startingValue) ? 0 : 1
+  
+  for (; i < sourceArray.length; i++) {
+    r = sourceFunction(sourceArray[i], r)
+  }
+  
+  return r;
+}
