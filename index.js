29898777d088e96cd1dfbd98d53142010ba246d0
Sarah <saranderson13@gmail.com>
2019-12-03 13:08:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+
+function map(array, callBack) {
+    let r = [];
+    for (const ea of array) { r.push(callBack(ea)) }
+    return r;
+}
+
+function reduce(array, callBack, startingPoint = undefined) {
+    function checkNum(element) { return typeof element === "number" }
+    let allNums = array.every(checkNum);
+    let r = allNums ? startingPoint || 0 : true;
+
+    for (const ea of array) { r = callBack(r, ea); }
+    return r;
+}
